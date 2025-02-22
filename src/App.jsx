9fd16cc01c1faa6a9611e17f29ff56567f6a4f9b
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-02-22 14:50:40 +0600

    here set up enverment for exclusive ccomarce website design

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
+import React from "react";
 
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      
-     
+      <h1 className="text-red-700">Nhaid</h1>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
